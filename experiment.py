ea1f82d9980014b8f658d9a3f93bcd58bd4a66f1
Kiran <kirandewit@gmail.com>
2025-11-16 23:57:23 +0100

    Restructured signal maker

--- experiment.py
+++ experiment.py
@@ -9,21 +9,21 @@ SAMPLING_RATE: int = 44100
 GAIN: float = 0.3
 
 stream = sd.OutputStream(samplerate=SAMPLING_RATE, blocksize=BLOCKSIZE, channels=1)
+stream.start()
 
 A_note = sm.PureSignal(SAMPLING_RATE, 440, 0.0, 3, "A")
 Csharp_note = sm.PureSignal(SAMPLING_RATE, 554.37, 0.0, 3, "Csharp")
 E_note = sm.PureSignal(SAMPLING_RATE, 659.26, 0.0, 3, "E")
 
-Amajor_chord = sm.NonPureSignal(np.array([A_note, Csharp_note, E_note]))
+Amajor_chord = sm.CompositeSignal(np.array([A_note, Csharp_note, E_note]))
 
-# plt.figure(figsize=(17,4))
-# plt.plot(A_note)
-# ax = plt.gca()
-# ax.set_ylim([-3,3])
-# ax.set_xlim([0, 440])
+plt.figure(figsize=(17,4))
+plt.plot(Amajor_chord.signal)
+ax = plt.gca()
+ax.set_ylim([-3,3])
+ax.set_xlim([0, 1000])
 # plt.show()
-print(Amajor_chord.combined_signal)
 
-stream.start()
-stream.write(np.ascontiguousarray(Amajor_chord.combined_signal, dtype='float32'))
+
+stream.write(np.ascontiguousarray(Amajor_chord.signal, dtype='float32'))
 
--- signal_maker.py
+++ signal_maker.py
@@ -1,13 +1,24 @@
 import numpy as np
 
-class PureSignal:
+class BaseSignal:
+    def __init__(self):
+        self.signal = np.array([], dtype=float)
+
+    def normalize(self):
+        if np.max(np.abs(self.signal)) != 0:
+            self.signal /= np.max(np.abs(self.signal))
+
+    def amplify(self, gain: float):
+        self.signal *= gain
+
+class PureSignal(BaseSignal):
     def __init__(self, sampling_rate: int, frequency: float, initial_phase: float = 0.0, duration: float = 1.0, label: str = "") -> None:
+        super().__init__()
         self.sampling_rate = sampling_rate
         self.frequency = frequency
-        self.current_phase = 0.0
+        self.current_phase = initial_phase
         self.label = label
 
-        self.signal: np.ndarray = np.array([], dtype=float)
         self.generate(duration)
 
     def generate(self, duration: float):
@@ -20,35 +31,29 @@ class PureSignal:
         self.current_phase =  (phase[-1] + 2*np.pi*self.frequency/self.sampling_rate) % (2*np.pi)
         self.signal = np.concatenate([self.signal, new_signal_segment])
         self.normalize()
-
-    def normalize(self):
-        self.signal /= np.max(np.abs(self.signal))
         
     def extend_duration(self, duration: float):
         self.generate(duration)
 
-    def amplify(self, gain: float = 1.0):
-        self.signal *= gain
     
-class NonPureSignal:
-    def __init__(self, signals: np.ndarray) -> None:
-        self.signals_array = signals
-
-        self.combined_signal = np.array([], dtype=float)
+class CompositeSignal(BaseSignal):
+    def __init__(self, signals = None, label: str = "") -> None:
+        super().__init__()
+        self.signals_array = signals if signals is not None else []
         self.sum_signals()
-    
-    def normalize(self):
-        self.combined_signal /= np.max(np.abs(self.combined_signal))
 
     def sum_signals(self):
         temp_array = np.array([x.signal for x in self.signals_array])
-        self.combined_signal = temp_array.sum(axis=0)
+        self.signal = temp_array.sum(axis=0)
         self.normalize()
         
 
-    def add_signal(self, signal: np.ndarray):
-        self.signals_array = np.concatenate([self.signals_array, signal])
-        self.sum_signals()
+    def add_signal(self, new_signal: np.ndarray, composition_method = "sum"):
+        self.signals_array = np.concatenate([self.signals_array, new_signal])
+        
+        match composition_method:
+            case "sum":   
+                self.sum_signals()
 
 
 
