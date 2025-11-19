e9156361c7f47c3d84dcb46b2f4439090f627c4f
Kiran <kirandewit@gmail.com>
2025-11-19 15:32:58 +0100

    Made normalizatin optional by adding parameter

--- signal_maker.py
+++ signal_maker.py
@@ -4,57 +4,79 @@ class BaseSignal:
     def __init__(self):
         self.signal = np.array([], dtype=float)
 
-    def normalize(self):
+    def normalize_signal(self):
         if np.max(np.abs(self.signal)) != 0:
             self.signal /= np.max(np.abs(self.signal))
 
-    def amplify(self, gain: float):
+    def amplify_signal(self, gain: float):
         self.signal *= gain
 
 class PureSignal(BaseSignal):
-    def __init__(self, sampling_rate: int, frequency: float, initial_phase: float = 0.0, duration: float = 1.0, label: str = "") -> None:
+    def __init__(self, sampling_rate: int, frequency: float, 
+                 amplitude: float, initial_phase: float = 0.0, 
+                 duration: float = 1.0, label: str = "",
+                 normalize: bool = True) -> None:
+        
         super().__init__()
         self.sampling_rate = sampling_rate
         self.frequency = frequency
-        self.current_phase = initial_phase
+        self.amplitude = amplitude
+        self.initial_phase = initial_phase
         self.label = label
+        self.normalize = normalize
 
-        self.generate(duration)
+        self.generate_signal(duration)
 
-    def generate(self, duration: float):
+    def generate_signal(self, duration: float):
         n_samples = int(self.sampling_rate * duration)
         time = np.arange(n_samples) / self.sampling_rate
 
-        phase = 2*np.pi*self.frequency*time + self.current_phase
-        new_signal_segment = np.sin(phase)
+        generated_signal = self.amplitude * np.sin(2*np.pi*self.frequency*time + self.initial_phase)
+        self.signal = generated_signal
 
-        self.current_phase =  (phase[-1] + 2*np.pi*self.frequency/self.sampling_rate) % (2*np.pi)
-        self.signal = np.concatenate([self.signal, new_signal_segment])
-        self.normalize()
+        if self.normalize:
+            self.normalize_signal()
         
-    def extend_duration(self, duration: float):
-        self.generate(duration)
+    def change_duration(self, duration: float):
+        if duration < 0:
+            raise Exception("Duration cannot be lower than 0")
+        self.generate_signal(duration)
 
     
 class CompositeSignal(BaseSignal):
-    def __init__(self, signals = None, label: str = "") -> None:
+    def __init__(self, signals = None, label: str = "", normalize: bool = True) -> None:
         super().__init__()
         self.signals_array = signals if signals is not None else []
+        self.label = label
+        self.normalize = normalize
         self.sum_signals()
 
     def sum_signals(self):
         temp_array = np.array([x.signal for x in self.signals_array])
         self.signal = temp_array.sum(axis=0)
-        self.normalize()
+        
+        if self.normalize:
+            self.normalize_signal()
         
 
-    def add_signal(self, new_signal: np.ndarray, composition_method = "sum"):
+    def add_signal(self, new_signal, composition_method = "match max"):
         self.signals_array = np.concatenate([self.signals_array, new_signal])
         
         match composition_method:
-            case "sum":   
-                self.sum_signals()
-
+            case "pad after":  
+                #TODO add padding after signal 
+                pass
+            case "pad before":
+                #TODO add padding before signal
+                pass
+            case "match min":
+                #TODO change self.signals_array to match smallest signal length
+                pass
+            case "match max":
+                #TODO change self.signals_array to match largest signal length
+                pass
+        
+        self.sum_signals()
 
 
 
